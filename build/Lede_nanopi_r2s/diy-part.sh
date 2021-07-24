6baf65b3cb5ded19b6356d414a260daa5f238836
shidahuilang <81285256+shidahuilang@users.noreply.github.com>
2021-07-24 22:03:31 +0800

    Update diy-part.sh

--- build/Lede_nanopi_r2s/diy-part.sh
+++ build/Lede_nanopi_r2s/diy-part.sh
@@ -27,7 +27,7 @@ sed -i "s/OpenWrt /大灰狼 $(TZ=UTC-8 date "+%Y.%m.%d") @ OpenWrt /g" $ZZZ    
 
 sed -i '/CYXluq4wUazHjmCDBCqXF/d' $ZZZ                                                            # 设置密码为空
 
-sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/rockchip/Makefile                              # 默认内核5.10，修改内核为5.4
+#sed -i 's/PATCHVER:=5.4/PATCHVER:=5.10/g' target/linux/rockchip/Makefile                              # 默认内核5.10，修改内核为5.4
 
 # K3专用，编译K3的时候只会出K3固件
 #sed -i 's|^TARGET_|# TARGET_|g; s|# TARGET_DEVICES += phicomm_k3|TARGET_DEVICES += phicomm_k3|' target/linux/bcm53xx/image/Makefile
